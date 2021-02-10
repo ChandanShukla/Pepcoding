a04b43356f736121c51382decc2f7333a292d5c4
ChandanShukla <chandanshukla269@gmail.com>
2021-02-10 12:05:58 +0530

    Create ZigZag_Recursion.java

---
+++ ZigZag_Recursion.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ZigZag_Recursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner (System.in);
+		int n = scn.nextInt();
+		
+		pzz(n);
+
+	}
+	
+	public static void pzz(int n) {
+		if(n==0) {
+			return ;
+		}
+		System.out.print(n);
+		pzz(n-1);
+		System.out.print(n);
+		pzz(n-1);
+		System.out.print(n);
+		
+		
+	}
+
+}
